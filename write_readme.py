8dce177749ea60610f953735c3a54a4b096e1e3d
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2021-11-29 15:52:22 -0500

    Update write_readme.py

--- write_readme.py
+++ write_readme.py
@@ -2,11 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 from jinja2 import Template
 from datetime import datetime
-
+from zoneinfo import ZoneInfo
 
 BLOG_URL = "https://mpewsey.github.io"
 README_TEMPLATE = "README_Template.md"
 README_PATH = "README.md"
+TIMEZONE = ZoneInfo("US/Eastern")
 
 
 def fetch_blog_post_links() -> list:
@@ -31,7 +32,7 @@ def write_readme():
         template = Template(fh.read())
 
     blog_posts = get_blog_posts_string()
-    date_time = datetime.utcnow().strftime("%d %b %Y, %H:%M UTC")
+    date_time = datetime.now().astimezone(TIMEZONE).strftime("%d %b %Y, %I:%M %p EST")
     readme = template.render(blog_posts = blog_posts, date_time = date_time)
 
     with open(README_PATH, "wt") as fh:
